3f066bdddfef7107f2086aa46abd30412431e046
Ferran Parareda <fredy87@gmail.com>
2017-10-17 21:37:43 +0200

    Save results in file Change of status once the game is done

--- src/main/java/AnkiLogic.java
+++ src/main/java/AnkiLogic.java
@@ -10,13 +10,11 @@ import java.util.Scanner;
  */
 public class AnkiLogic {
 
+    public static final String ANSI_BOLD = "\u001B[1m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public List<Card> loadCardsStatus(String fileName){
         List<Card> listOfCards = AnkiUtils.readCardsFromFile(fileName);
-
-//        System.out.println("Loaded these cards:");
-//        for(Card card : listOfCards){
-//            System.out.println(card.getStatus() + "\t | " + card.getQuestion() + "\t | " + card.getAnswer());
-//        }
         return listOfCards;
     }
 
@@ -42,12 +40,30 @@ public class AnkiLogic {
     }
 
     public void endGame(String fileName, List<Card> listOfCards){
-        AnkiUtils.saveStatusFile(fileName, listOfCards);
-
         if(AnkiUtils.allTheCardsSettled(listOfCards)){
-            System.out.println("Congratulate! The cards are studied properly!");
+            System.out.println(ANSI_BOLD + "\nCongratulate! The cards are studied properly!" + ANSI_RESET);
         } else {
-            System.out.println("Good job! Goodbye!");
+            updateCardsWithRules(listOfCards);
+            System.out.println(ANSI_BOLD + "\nGood job! Goodbye!" + ANSI_RESET);
+        }
+        AnkiUtils.saveStatusFile(fileName, listOfCards);
+    }
+
+
+    /**
+     * Based on the rules, when the session is over :
+     *
+     * The cards in the red box will be studied again the same day
+     * The cards in the orange box will be studied again the next (we can put it into red box)
+     * The cards in the green box, will be studied again two days later (we can put it into the orange box)
+     */
+    public void updateCardsWithRules(List<Card> listOfCards) {
+        for(Card card : listOfCards){
+            if(CardStatus.GREEN_BOX.name().equals(card.getStatus().name())){
+                card.setStatus(CardStatus.ORANGE_BOX);
+            } else if(CardStatus.ORANGE_BOX.name().equals(card.getStatus().name())){
+                card.setStatus(CardStatus.RED_BOX);
+            }
         }
     }
 }
